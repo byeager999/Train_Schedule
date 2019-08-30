b63814d745315e962691932aa4ec24463a701430
Brian Yeager <rbrianyeager@gmail.com>
2019-08-29 20:08:05 -0400

    js logic mostly complete.  Need to add time calc and IDs to html

---
+++ assets/javascript/train_app.js
@@ -0,0 +1,84 @@
+// initialize Firebase
+var config = {
+    apiKey: "",
+    authDomain: "train-schedule-59ce8.firebaseio.com",
+    database: "https://train-schedule-59ce8.firebaseio.com",
+    storageBucket: "train-schedule-59ce8.appspot.com"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// button for adding trains
+
+$("#add-train-btn").on("click", function(event){
+    event.preventDefault();
+
+    // captures user input
+    // NEED WAY TO CAPTURE 24 HR TIME OR CONVERT TIME TO 24 HR
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var trainFirst = $("#firstArrive-input").val().trim(), 
+    var trainFreq = $("#frequency-input").val().trim();
+
+
+    // temp object for holding input data
+    var newTrain = {
+        name: trainName,
+        destination: trainDest,
+        firstArrive: trainFirst,
+        frequency: trainFreq
+    };
+
+    // uploads train data to db
+    database.ref().push(newTrain);
+
+    // logs to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstArrive);
+    console.log(newTrain.frequency);
+
+    alert("Train successfully added")
+
+    // clears text boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#firstArrive-input").val("");
+    $("#frequency-input").val("");
+});
+
+// firebase event for adding new train to the db and a row in the html when user adds a train
+database.ref().on("child_added", function(childSnapshot){
+    console.log(childSnapshot.val());
+
+    // storing into a variable
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var trainFirst = childSnapshot.val().firstArrive;
+    var trainFreq = childSnapshot.val().frequency;
+
+    // logs to console
+    console.log(trainName);
+    console.log(trainDest);
+    console.log(trainFirst);
+    console.log(trainFreq);
+
+    // format first train
+
+    // calculate next train arrival time
+
+    // create new row
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDest),
+        $("<td>").text(trainFreq),
+        $("<td>").text(trainNext),
+        $("<td>").text(trainMinAway),
+    );
+
+    $("#train-table > tbody").append(newRow);
+
+
+});
