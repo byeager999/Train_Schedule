1dddbd234df05de39e8a78810f5ab61b16945532
Brian Yeager <rbrianyeager@gmail.com>
2019-09-02 15:44:31 -0400

    all completed just need to fix the mins away calc and turn time to military time

--- assets/javascript/train_app.js
+++ assets/javascript/train_app.js
@@ -1,17 +1,20 @@
 // initialize Firebase
+// Your web app's Firebase configuration
 var config = {
-    apiKey: "",
-    authDomain: "train-schedule-59ce8.firebaseio.com",
-    database: "https://train-schedule-59ce8.firebaseio.com",
-    storageBucket: "train-schedule-59ce8.appspot.com"
+    apiKey: "AIzaSyAQn3ix5RxyJqdOPPE-OqhY6Z-vm2hJv9E",
+    authDomain: "train-schedule-59ce8.firebaseapp.com",
+    databaseURL: "https://train-schedule-59ce8.firebaseio.com",
+    projectId: "train-schedule-59ce8",
+    storageBucket: "",
+    messagingSenderId: "636841096373",
+    appId: "1:636841096373:web:58ef5e5f4de5d023"
 };
-
+// Initialize Firebase
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
 // button for adding trains
-
 $("#add-train-btn").on("click", function(event){
     event.preventDefault();
 
@@ -19,7 +22,8 @@ $("#add-train-btn").on("click", function(event){
     // NEED WAY TO CAPTURE 24 HR TIME OR CONVERT TIME TO 24 HR
     var trainName = $("#train-name-input").val().trim();
     var trainDest = $("#destination-input").val().trim();
-    var trainFirst = $("#firstArrive-input").val().trim(), 
+    // THIS IS PROBABLY WRONG
+    var trainFirst = moment($("#firstArrive-input").val().trim(), "HH:mm").format("X");
     var trainFreq = $("#frequency-input").val().trim();
 
 
@@ -68,32 +72,30 @@ database.ref().on("child_added", function(childSnapshot){
     // format first train
 
     // calculate next train arrival time
-    var tFrequency = 5;
 
-    var firstTime = "02:00";
 
-    var firstTimeConverted = moment(trainFirst, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
+    var trainFirstConverted = moment(trainFirst, "HH:mm").subtract(1, "years");
+    console.log(trainFirstConverted);
 
     // current time
     var currentTime = moment();
     console.log("Current Time: " + moment(currentTime).format("hh:mm"));
 
     // difference between times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var diffTime = moment().diff(moment(trainFirstConverted), "minutes");
     console.log("Difference in time: " + diffTime);
 
     // time apart
-    var tRemainder = diffTime % tFrequency;
+    var tRemainder = diffTime % trainFreq;
     console.log(tRemainder);
 
     // minutes away
-    var minsTillTrain = tFrequency - tRemainder;
+    var minsTillTrain = trainFreq - tRemainder;
     console.log("Minutes till train: " + minsTillTrain);
 
     // next train
-    var nextTrain = momment().add(minsTillTrain, "minutes");
-    console.log("Arrival time: " + moment(nestTrain).format("hh:mm"));
+    var trainNext = moment().add(minsTillTrain, "minutes");
+    console.log("Arrival time: " + moment(trainNext).format("hh:mm"));
 
     // create new row
     var newRow = $("<tr>").append(
@@ -101,7 +103,7 @@ database.ref().on("child_added", function(childSnapshot){
         $("<td>").text(trainDest),
         $("<td>").text(trainFreq),
         $("<td>").text(trainNext),
-        $("<td>").text(trainMinAway),
+        $("<td>").text(minsTillTrain),
     );
 
     $("#train-table > tbody").append(newRow);
